6a48b588b4e12b77db1f6052c853aa00bdf67df4
elite <apipath90@gmail.com>
2026-03-25 14:01:28 +0700

    v444jj

--- frontend-estimates/src/pages/admin/subject/subjectedit.tsx
+++ frontend-estimates/src/pages/admin/subject/subjectedit.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import  { useState } from "react";
 
 // ── Types ────────────────────────────────────────────────────────────
 type Branch = "CS" | "ITII" | "GIS" | "AI" | "CY";
